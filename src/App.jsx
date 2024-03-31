0e88b45599a787ba2f29e3a2ccd3d9cae35a8cc3
MNZ02 <abdulminhaz47@gmail.com>
2024-03-31 16:09:18 +0530

    Add Shows component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Catalog from "./components/Catalog"
 import Body from "./components/Body"
 import Events from "./components/Events"
+import Shows from "./components/Shows"
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Catalog />
       <Body />
+      <Shows />
       <Events />
     </>
   )
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -4,6 +4,9 @@ import rightArrowBlackSvg from '../assets/right-arrow-black.svg';
 
 function Events() {
     const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
 
     const converturl = (url) => {
         const id = url.match(/\/d\/([-\w]+)/)[1];
@@ -22,23 +25,44 @@ function Events() {
         return new Date(dateString).toLocaleDateString('en-US', options);
     };
 
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`https://gg-backend-assignment.azurewebsites.net/api/Events?code=FOX643kbHEAkyPbdd8nwNLkekHcL4z0hzWBGCd64Ur7mAzFuRCHeyQ==&page=${page}&type=upcoming`);
+            const data = await response.json();
+            if (data.events.length === 0) {
+                setHasMore(false);
+            } else {
+                setEvents((prevEvents) => [...prevEvents, ...data.events]);
+                setPage((prevPage) => prevPage + 1);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight && hasMore) {
+            fetchData();
+        }
+    };
+
     useEffect(() => {
         fetchData();
     }, []);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch('https://gg-backend-assignment.azurewebsites.net/api/Events?code=FOX643kbHEAkyPbdd8nwNLkekHcL4z0hzWBGCd64Ur7mAzFuRCHeyQ==&page=1&type=upcoming');
-            const data = await response.json();
-            console.log(data);
-            setEvents(data.events);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [hasMore]);
 
     return (
-        <div className='flex flex-wrap -space-x-14 ml-48'>
+        <div className='flex flex-wrap -space-x-14 ml-48 mt-8'>
             <div className='w-full'>
                 <div className='flex space-x-2 -ml-12'>
                     <h1 className='font-semibold text-lg ml-24'>Upcoming Events</h1>
@@ -46,14 +70,14 @@ function Events() {
                 </div>
             </div>
 
-            {events.map((event) => (
-                <div key={event.eventName} className='m-2 p-2'>
+            {events.map((event, index) => (
+                <div key={index} className='m-2 p-2'>
                     <div className='m-2 p-2 border border-gray-300 rounded-md w-fit ml-20 relative'>
-                        <img className='w-80 rounded-md' src={converturl(event.imgUrl)} alt="img" />
+                        <img className='w-80 rounded-md cursor-pointer' src={converturl(event.imgUrl)} alt="img" />
                         <div className='border border-black rounded-md px-[12px] py-[7px] mx-4 absolute -mt-[50px]  max-w-[290px] bg-black w-full opacity-70'>
                             <p className='text-sm px-2 text-white'>{formatDate(event.date)}</p>
                         </div>
-                        <p className='font-semibold text-lg mt-4 px-2'>{event.eventName}</p>
+                        <p className='font-semibold text-lg mt-4 px-2 cursor-pointer'>{event.eventName}</p>
                         <div className='flex justify-between'>
                             <div className='flex space-x-2 px-2 my-2 text-sm'>
                                 <img className='w-3' src={locationSvg} alt="location" />
@@ -66,6 +90,7 @@ function Events() {
                     </div>
                 </div>
             ))}
+            {loading && <p>Loading...</p>}
         </div>
     );
 }
